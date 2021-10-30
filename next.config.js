9e6c8ea5688a01cb6d3dcab65759dcc3f3650278
Hayoung <gkduddy0814@gmail.com>
2021-10-31 01:34:23 +0900

    feat : next/tailwind/redux.. initail setting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.resolve.modules.push(__dirname); // 추가
+    return config;
+  },
+
+  env: {
+    LOCAL_SERVER: 'http://localhost:3000/api',
+    // PROD_SERVER: 'https://insta-frontend.vercel.app/api',
+  },
+};
