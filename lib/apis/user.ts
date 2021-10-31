360cdd46210e4df396d81c0f668a2b38269a84aa
Hayoung <gkduddy0814@gmail.com>
2021-10-31 20:33:10 +0900

    feat : add fetchuser and wronganswer apis

--- lib/apis/user.ts
+++ lib/apis/user.ts
@@ -1,7 +1,17 @@
 import { NEXT_SERVER } from 'config';
-import { User } from 'types/user';
+import { Wrong, User } from 'types/user';
 import fetcher from './fetcher';
 
-export default async function fetchUserList() {
-  return (await fetcher(`${NEXT_SERVER}/v1/user`)) as User[];
+export async function fetchUserList() {
+  return (await fetcher(`${NEXT_SERVER}/v1/users`)) as User[];
+}
+
+export async function fetchUser(id: string) {
+  return (await fetcher(`${NEXT_SERVER}/v1/user/${id}`)) as User;
+}
+
+export async function fetchWrongAnswers(id: string) {
+  return (await fetcher(
+    `${NEXT_SERVER}/v1/user/wrong/answers?id=${id}`,
+  )) as Wrong[];
 }
