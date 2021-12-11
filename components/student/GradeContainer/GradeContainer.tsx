452e6b4c60d556052b0245a58fd26f52805c8f80
Hayoung <gkduddy0814@gmail.com>
2021-12-11 20:07:07 +0900

    feat : add open modal feature

--- components/student/GradeContainer/GradeContainer.tsx
+++ components/student/GradeContainer/GradeContainer.tsx
@@ -1,14 +1,20 @@
 import Button from '@material-ui/core/Button';
+import { openWriteModal } from 'lib/redux/modal/modalSlice';
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 interface Props {}
 
 const GradeContainer: React.FC<Props> = ({}) => {
+  const dispatch = useDispatch();
+
   return (
     <>
       <div className="my-2">성적관리</div>
       <div className="flex justify-end">
-        <Button>성적추가</Button>
+        <Button onClick={() => dispatch(openWriteModal('grade'))}>
+          성적추가
+        </Button>
       </div>
       <table className="w-full">
         <colgroup>
