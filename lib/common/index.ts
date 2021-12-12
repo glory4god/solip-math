c7d390b9dfb84b78f1a4f56332f9f97fe88f032f
Hayoung <gkduddy0814@gmail.com>
2021-12-13 01:13:51 +0900

    feat : add string to html feature

--- lib/common/index.ts
+++ lib/common/index.ts
@@ -4,3 +4,17 @@ export function mongoDateFormatting(date: Date) {
   const day = date.toString().split('-')[2].substr(0, 2);
   return `${year}-${month}-${day}`;
 }
+
+export function stringToHTMLFormatting(str: string) {
+  return {
+    __html: str
+      .split('\n')
+      .map((content: string, idx) => {
+        return `<span key={${idx}}>
+          ${content}
+          <br />
+        </span>`;
+      })
+      .join(''),
+  };
+}
