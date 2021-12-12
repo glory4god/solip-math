69e05489a24fd9b0da314c6eca876bd6d166817c
Hayoung <gkduddy0814@gmail.com>
2021-12-13 01:12:46 +0900

    feat : add button props

--- components/manage/UserContent/UserContent.tsx
+++ components/manage/UserContent/UserContent.tsx
@@ -2,57 +2,99 @@ import React from 'react';
 
 import Button from '@material-ui/core/Button';
 import { User } from 'types/user';
+import { NEXT_SERVER } from 'config';
+import { useRouter } from 'next/dist/client/router';
+
+import cn from 'classnames';
 
 interface ContentProps {
   className?: string;
-  condition: string;
   handleCurrIdx: () => void;
   grade: string;
   count: number;
   isShow: boolean;
   userList: User[];
-  handleUser: (_id: string) => void;
+  buttonProps: { condition: string; buttonHandler: (_id: string) => void }[];
 }
 
 const UserContent: React.FC<ContentProps> = ({
   className,
-  condition,
   grade,
   handleCurrIdx,
   count,
   isShow,
   userList,
-  handleUser,
+  buttonProps,
 }) => {
+  const router = useRouter();
+
+  const deleteGradeHandler = async (grade: string) => {
+    const res = await fetch(`${NEXT_SERVER}/v1/grade/post?id=${grade}`, {
+      method: 'DELETE',
+    });
+    if (!res.ok) {
+      return alert('반 삭제를 실패했습니다.');
+    }
+
+    router.replace(router.asPath);
+    return alert('반을 삭제했습니다.');
+  };
+
   return (
     <div className="flex py-2 flex-col justify-center">
       <a onClick={handleCurrIdx}>
-        <b>{grade}</b> [{count}명]
+        <b>{grade}</b> [{count}명]{' '}
+        <span
+          style={{ fontSize: '10px' }}
+          onClick={(e) => {
+            e.stopPropagation();
+            const result = confirm(`${grade}반을 삭제하시겠습니까?`);
+            if (result) {
+              deleteGradeHandler(grade);
+            }
+          }}>
+          반삭제
+        </span>
       </a>
-      {isShow &&
-        userList.map((user) => {
-          if (user.grade === grade)
-            return (
-              <div
-                className="flex justify-between items-center"
-                key={user.name}>
-                <span>{user.name}</span>
-                <Button
-                  variant="outlined"
-                  color="secondary"
-                  onClick={() => {
-                    const result = confirm(
-                      `${user.name}학생을 ${condition}하시겠습니까?`,
-                    );
-                    if (result) {
-                      handleUser(user._id);
-                    }
-                  }}>
-                  <a>{condition}</a>
-                </Button>
-              </div>
-            );
-        })}
+      <div
+        className={cn(
+          `removeScroll`,
+          `${
+            isShow ? 'max-h-72' : 'max-h-0'
+          } transition:max-height duration-500`,
+        )}>
+        {isShow &&
+          userList.map((user) => {
+            if (user.grade === grade)
+              return (
+                <div
+                  className={`flex justify-between items-center`}
+                  key={user.name}>
+                  <span>{user.name}</span>
+                  <div>
+                    {buttonProps.map((button) => {
+                      return (
+                        <Button
+                          key={button.condition}
+                          variant="outlined"
+                          color="secondary"
+                          onClick={() => {
+                            const result = confirm(
+                              `${user.name}학생을 ${button.condition}하시겠습니까?`,
+                            );
+                            if (result) {
+                              button.buttonHandler(user._id);
+                            }
+                          }}>
+                          <a>{button.condition}</a>
+                        </Button>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+          })}
+      </div>
     </div>
   );
 };
