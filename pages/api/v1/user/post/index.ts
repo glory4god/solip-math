4ddad9f139edc85853468c75ce6fa831ec5b78b7
Hayoung <gkduddy0814@gmail.com>
2021-12-19 02:40:32 +0900

    refactor : refactoring patch api feature

--- pages/api/v1/user/post/index.ts
+++ pages/api/v1/user/post/index.ts
@@ -4,17 +4,30 @@ import User from 'backend/mongoDB/models/User';
 import WrongAnswer from 'backend/mongoDB/models/WrongAnswer';
 import Management from 'backend/mongoDB/models/StudentManagement';
 
+interface User {
+  name: string;
+  grade: string;
+  gender: '남' | '여' | '';
+}
+
+interface UserEdit {
+  _id: string;
+  name: string;
+  auth: boolean;
+  grade: string;
+  gender: '남' | '여' | '';
+  createdDate: Date;
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   await dbConnect();
 
-  const edit = req.body;
-
-  const { userId } = req.query;
-
   if (req.method === 'POST') {
+    const edit = req.body as User;
+
     User.findOne({ name: edit.name }, (err: any, user: any) => {
       if (user) {
         return res
@@ -38,7 +51,9 @@ export default async function handler(
       }
     });
   } else if (req.method === 'DELETE') {
-    User.findByIdAndDelete(userId, (err: any, user: any) => {
+    const edit = req.body as UserEdit;
+
+    User.findByIdAndDelete(edit._id, (err: any, user: any) => {
       if (err) {
         return res.status(400).json({ status: 400, message: 'delete failed' });
       } else {
@@ -67,12 +82,15 @@ export default async function handler(
       }
     });
   } else if (req.method === 'PATCH') {
-    console.log('시작');
-    let user = await User.findById(userId);
-    user.auth = !user.auth;
+    const edit = req.body as UserEdit;
+
+    let user = await User.findById(edit._id);
+    user.name = edit.name;
+    user.grade = edit.grade;
+    user.auth = edit.auth;
+    user.gender = edit.gender;
     await user.save();
-    return res
-      .status(200)
-      .json({ status: 200, message: 'user auth patch success' });
+
+    return res.status(200).json({ status: 200, message: 'user patch success' });
   }
 }
